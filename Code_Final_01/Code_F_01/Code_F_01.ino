5b815bca285703fc58fc676ccb9f176f210fcee3
Maruf <mesbahulmaruf@iut-dhaka.edu>
2020-11-11 18:44:23 +0600

    First Version

---
+++ Code_Final_01/Code_F_01/Code_F_01.ino
@@ -0,0 +1,42 @@
+#define BLYNK_PRINT Serial    
+#include <SPI.h>
+#include <ESP8266WiFi.h>       
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+char auth[] = "gK6W0QJJGJ_byUfwoPjaDoOm_x7s3qaa";
+char ssid[] = "Hell Room";  
+char pass[] = "203203203";
+#define DHTPIN D2
+#define DHTTYPE DHT11 
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void setup() {
+  Serial.begin(9600); 
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  
+  timer.setInterval(2000, sendSensor);
+}
+
+void sendSensor() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }  
+  Blynk.virtualWrite(V3, h); 
+  Blynk.virtualWrite(V2, t);
+}
+
+
+void loop() {
+  Blynk.run(); 
+  timer.run(); 
+
+}
